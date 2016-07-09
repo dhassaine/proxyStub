43d5688cd5e2e27bd532199137d0617a7d9249df
Djamel Hassaine <dhassaine@gmail.com>
2016-07-09 19:10:54 +0100

    removed comments

--- proxy-stub.js
+++ proxy-stub.js
@@ -1,26 +1,3 @@
-/*
-export default function traceMethodCalls(obj) {
-  let numberOfTimesCalled = 0;
-  const handler = {
-    get(target, key) {
-      if (typeof target[key] !== 'function') {
-        return target[key];
-      }
-
-      return (...args) => (
-        numberOfTimesCalled++ ,
-        target[key].apply(target, args)
-      );
-    }
-  };
-
-  return {
-    proxy: new Proxy(obj, handler),
-    callCount: () => numberOfTimesCalled
-  };
-}
-*/
-
 export default function traceMethodCalls({obj, stubMethod = false, throws=false}) {
   let numberOfTimesCalled = 0;
   let lastPropertyAccessed = void 0;
